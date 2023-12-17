00568d73c093e746430f024ff48cbfd6175128d1
Ravi Kumar <Ravikumar-2021>
2023-12-17 20:54:18 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import  Sliders  from './Components/Carousel/Sliders'
+import Navbar from './Components/Navigation/Navbar'
+
+import { Fragment as Fragment2, useState, react } from "react";
+
+function App() {
+
+  return (
+   <>
+    <Navbar/>
+    <Sliders/>
+   </>
+  )
+}
+
+export default App
